5190b647a3003e96c817a0c94156f27cc5f9b1d4
Mario <mariosmilcic@gmail.com>
2022-02-20 18:32:30 +0100

    added tailwindcss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,32 @@
-import React from 'react';
-
-import { NavLink } from 'react-router-dom';
-import '../app.css';
-import styles from '../Navbar.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
     {
       id: 1,
-      path: '/',
-      text: 'Home',
+      path: "/",
+      text: "Home",
     },
     {
       id: 2,
-      path: '/about',
-      text: 'About',
+      path: "/about",
+      text: "About",
     },
   ];
 
   return (
-    <nav className={styles.navbar}>
-      <ul>
+    <nav className="w-full h-20 bg-sky-600 py-0 px-8 border-b border-solid border-white">
+      <ul className="h-full list-none flex p-o m-0 items-center justify-center">
         {links.map((link) => {
           return (
-            <li key={link.id}>
-              <NavLink to={link.path} activeClassName={styles.active} exact>
+            <li key={link.id} className="mx-4">
+              <NavLink
+                to={link.path}
+                className="nav"
+                activeClassName="font-black"
+                exact
+              >
                 {link.text}
               </NavLink>
             </li>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#17515a",
+        secondary: "#ececec",
+        inputColor: "#95bcf0",
+      },
+      boxShadow: {
+        shadow: "0 1px 6px 0 rgba(0, 0, 0, 0.38)",
+      },
+    },
+  },
+  plugins: [],
+};
